bbe603734a587187c5a18dcb40da340013ff4778
ohmyizzy <conneri19@barringtonschools.org>
2018-07-11 13:51:55 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -32,5 +32,6 @@ def last_student_by_method
 end
 
 def first_second_and_third_students
-  # Write a solution that returns the first, second and third students
+  # Write a solution 
+  result = [STUDENT_NAMES.first, STUDENT_NAMES[1]. STUDENT_NAMES[2]]
 end
